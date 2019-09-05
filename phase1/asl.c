a74b9fa905912cc24130b7a6ae34686af617a80c
stonen2 <stonen2@xavier.edu>
2019-09-04 21:40:32 -0400

    Testing Fucking Out Blocked

--- phase1/asl.c
+++ phase1/asl.c
@@ -119,12 +119,7 @@ if semAdd is not found on the ASL or if the process queue associated with semAdd
 pcb_t *headBlocked(int *semAdd){
    semd_t * temp; 
    temp = searchForParent(semAdd);
-   if(temp -> s_next ->s_semAdd == semAdd){
-       return headProcQ(temp -> s_next -> s_procQ);
-   }
-   else{
-       return NULL; 
-   }
+  return headProcQ(temp->s_next->s_procQ);
 
 
 }
