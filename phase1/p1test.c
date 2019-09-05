baf1a2cb1c3ac6a259330744efa58d5941f1e2b2
stonen2 <stonen2@xavier.edu>
2019-09-04 20:51:01 -0400

    Adding Head Blocked

--- phase1/p1test.c
+++ phase1/p1test.c
@@ -287,6 +287,7 @@ void main() {
 		if (insertBlocked(&sem[i-10], q))
 			adderrbuf("insertBlocked(3): unexpected TRUE   ");
 	}
+	
 	if (removeBlocked(&sem[11]) != NULL)
 		adderrbuf("removeBlocked: removed nonexistent blocked proc   ");
 	addokbuf("insertBlocked and removeBlocked ok   \n");
