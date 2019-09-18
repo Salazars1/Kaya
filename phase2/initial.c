0d888e63c6aa95fedb07875925ddeff29aca8822
Santiago Salazar <salazars1@xavier.edu>
2019-09-17 23:15:42 -0400

    added scheduler.c logic and started initial.c logic

--- phase2/initial.c
+++ phase2/initial.c
@@ -1,3 +1,13 @@
+/*  PHASE 2
+    Written by NICK STONE AND SANTIAGO SALAZAR
+    Base code and Comments from PROFESSOR MIKEY G 
+    Finished on  
+*/
+
+/*********************************************************************************************
+                            Module Comment Section
+
+**********************************************************************************************/
 #include "../h/const.h"
 #include "../h/types.h" 
 #include "../e/asl.e"
@@ -40,16 +50,26 @@ int main(){
 
     /*  p = allocatePCB()
             -initialize the p_s
-            -stack pointer to the penultimate page of ohysical memory
+            -stack pointer to the penultimate page of physical memory
             -PC(p2test)
             -status:    -VM off
                         -Interupts Enabled
                         -Supervisor Mode on
+            
     */
-    
 
 
+
+
+
+    processCount++;              /* Adds one more process to the process count */
+    insertProQ(&readyQue, p);    /* Inserts the proces into the pcb data structure */
     
+    currentProcess = NULL;
+    
+    /* Lets the scheduler file take over.*/
+    scheduler();
+        
     return 0;
     
 
---
+++ phase2/scheduler.c
@@ -0,0 +1,59 @@
+/*  PHASE 2
+    Written by NICK STONE AND SANTIAGO SALAZAR
+    Base code and Comments from PROFESSOR MIKEY G 
+    Finished on  
+*/
+
+/*********************************************************************************************
+                            Module Comment Section
+
+**********************************************************************************************/
+#include "../h/const.h"
+#include "../h/types.h"
+#include "../e/initial.e"
+#include "../e/pcb.e"
+
+/* Variables that the scheduler uses from initial.c*/
+int processCount;
+int softBlockCount;
+pcb_t *currentProcess;
+pcb_t *readyQue;
+
+/*  Nucleus guarantees finite progress (NO STARVATION), therefore, every ready process
+    will have an opportunity to execute. For simplicity’s sake this will be a simple round-robin
+    scheduler with a time slice value of 5 milliseconds.
+    Parameters: 
+    Retrun:     */
+void scheduler()
+{
+
+    /*  Process was running and either was blocked or its pointer got 
+        removed from readyQue */
+    if (currProc != NULL)
+    {
+        /*DO STUFF*/
+    }
+
+    else if () /*  Starts next process in Queue*/
+    {
+        currentProcess = removeProcQ(&readyQue);
+    }
+
+    else
+    {
+        currentProcess = NULL; /* no process is running*/
+
+        if (processCount == 0)
+        { /* Everything finished running correctly */
+            HALT();
+        }
+        else if (procCount > 0 && sftBlkCount == 0)
+        { /* DEADLOCK CASE */
+            PANIC();
+        }
+        else if (procCount > 0 && sftBlkCount > 0)
+        { /* Processor is twiddling its thumbs */
+            WAIT();
+        }
+    }
+}
