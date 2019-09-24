2036a08e0ff731494b3fb7d3752b084603a1cae9
stonen2 <stonen2@xavier.edu>
2019-09-24 19:13:46 -0400

    Making the SysCallHandler

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -29,6 +29,52 @@ HIDDEN void Syscall8();
 
 /*Commenting the Logic of the some of the functions */
 void SYSCALLHandler(){
+/**
+ * There are 8 System calls that our Handler must look out for 
+ * Of these first 8 System calls the Kernel Mode must be active
+ * In order for these commands to execute 
+ * 
+*/
+state_t PTR PrevState = (state_t PTR) SYSCALLOLDAREA;
+
+
+
+if(PrevState->s_a0 > 9){
+    PassUpOrDie();
+/*Pass up Or Die */
+
+
+}
+
+if(PrevState -> s_a0 <= 8){
+    if(PrevState -> s_status == UMOFF){
+        /*Not Privileged Die*/
+    }
+    else{
+        /*Switch Statement for the first 8 Sys calls begins */
+
+
+
+
+
+    }
+}
+
+
+
+/**
+ * If the System Call is 9 -255 Meaning that it is not one of the Sys calls that we are implementing
+ * Then we are going to Pass up or Die 
+ * Then we know that if the sys call numbers are values 1-8 then we need to be in kernel mode to execute 
+*/
+
+
+
+
+
+
+
+
 
 }
 
@@ -43,4 +89,6 @@ void TLBTrapHandler(){
     
 }
 
+void PassUpOrDie(){
 
+}
