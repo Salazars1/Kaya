b8a3cfe72aaa353f4457e4aa73eacaf566652fb4
stonen2 <stonen2@xavier.edu>
2019-10-12 12:41:22 -0400

    Fixing Syntax

--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -18,6 +18,24 @@
 
 void IOTrapHandler(){
 
+/**
+ * Step 1 in the Interrupt handler is to Find which Line is causing the Interrupt. 
+ * 
+ * Line 0 - Multi core - Doesnt matter
+ * Line 1 & 2 - Clocks
+ * Line 3 Disk Device 
+ * Line 4 Tape Device 
+ * Line 5 Network Device
+ * Line 6 Printer Device 
+ * Line 7 Terminal Device 
+ * 
+ * 
+ * 
+*/
+
+
+
+
 
     
 }
