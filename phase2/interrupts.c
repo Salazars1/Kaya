10e990139eb4d9e38dc53149460748a01c0d63e3
Santiago Salazar <salazars1@xavier.edu>
2019-10-14 19:00:55 -0400

    added global variables to interrupts

--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -30,27 +30,31 @@ extern cpu_t TODStart;
 
 
 extern void CtrlPlusC(state_PTR oldstate, state_PTR NewState);
-HIDDEN int Gr
+HIDDEN int findDevice(int lineNumber);
 
 
 void IOTrapHandler()
 {
 
-    unsigned int OffendingLine;
-    int Linenumber;
+    unsigned int offendingLine;    
+    int lineNumber;
     int devicenumber;
+
     cpu_t timeInterruptOccurs;
     devregarea_t *OffendingDevice;
+    
     state_PTR caller;
     caller = (state_t *)INTERRUPTOLDAREA;
-    OffendingLine = caller->s_cause << 8 | 2;
+    
+    
     StoreTime(timeInterruptOccurs);
 
+    OffendingLine = caller->s_cause << 8 | 2;
+    
+
     if ((OffendingLine & MULTICORE) == MULTICORE)
-    {
-        /*Mutli Core is on */
+    {   /*Mutli Core is on */
         PANIC();
-        HALT();
     }
     else if ((OffendingLine & CLOCK1) == CLOCK1)
     {
@@ -63,40 +67,46 @@ void IOTrapHandler()
     else if ((OffendingLine & DISKDEVICE) == DISKDEVICE)
     {
         /*Disk Device is on  */
-        Linenumber = DI;
+        lineNumber = DI;
         OffendingDevice = (device_t *)0x1000003c;
     }
     else if ((OffendingLine & TAPEDEVICE) == TAPEDEVICE)
     {
         /*Tape Device is on */
-        Linenumber = TI;
+        lineNumber = TI;
     }
 
     else if ((OffendingLine & NETWORKDEVICE) == NETWORKDEVICE)
     {
         /*Network Device is on */
-        Linenumber = NETWORKI;
+        lineNumber = NETWORKI;
     }
     else if ((OffendingLine & PRINTERDEVICE) == PRINTERDEVICE)
     {
         /*Printer Device is on */
 
-        Linenumber = PRINTERI;
+        lineNumber = PRINTERI;
     }
     else if ((OffendingLine & TERMINALDEVICE) == TERMINALDEVICE)
     {
         /*Terminal Device is on */
-        Linenumber = TERMINALI;
+        lineNumber = TERMINALI;
     }
     else
     {
-
         PANIC();
     }
+    
     int devn;
 
     devn = finddevice(Linenumber);
     /*with Dev Reg and Line number Do literal magic*/
+
+    if(devn == -1){
+        PANIC();
+    }
+
+    
 
     /*To Continue Watch Part 2 and 3 of Interrupts but I have alot of questions to ask */
 
