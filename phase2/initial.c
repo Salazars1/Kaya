a7d6586942f6c6c612b35d11d1b983c807070e4b
Santiago Salazar <salazars1@xavier.edu>
2019-10-17 14:36:02 -0400

    didnt work

--- phase2/initial.c
+++ phase2/initial.c
@@ -59,25 +59,25 @@ int main()
 
     /* SYSCALL BREAK*/
     newLocation = (state_t *)SYSCALLNEWAREA;
-    newLocation->s_pc = (memaddr) SYSCALLHandler();     
+    newLocation->s_pc = (memaddr) SYSCALLHandler;     
     newLocation->s_sp = RAMTOP;
     newLocation->s_status = ALLOFF;  /* Turns the VMOFF, IMON, UMOFF (Checks const.h for info in the names) */
 
     /* PROGRAM TRAP */
     newLocation = (state_t*) PRGMTRAPNEWAREA;
-    newLocation->s_pc = (memaddr) PrgTrapHandler();
+    newLocation->s_pc = (memaddr) PrgTrapHandler;
     newLocation->s_sp = RAMTOP;
     newLocation->s_status = ALLOFF; /* Turns the VMOFF, IMON, UMOFF (Checks const.h for info in the names) */
 
     /* TLB MANAGEMENT */
     newLocation = (state_t *)TBLMGMTNEWAREA;
-    newLocation->s_pc = (memaddr) TLBTrapHandler();
+    newLocation->s_pc = (memaddr) TLBTrapHandler;
     newLocation->s_sp = RAMTOP;
     newLocation->s_status = ALLOFF; /* Turns the VMOFF, IMON, UMOFF (Checks const.h for info in the names) */
 
     /* INTERRUPTS */
     newLocation = (state_t *)INTERRUPTNEWAREA;
-    newLocation->s_pc = (memaddr) IOTrapHandler();
+    newLocation->s_pc = (memaddr) IOTrapHandler;
     newLocation->s_sp = RAMTOP;
     newLocation->s_status = ALLOFF; /* Turns the VMOFF, IMON, UMOFF (Checks const.h for info in the names) */
 
