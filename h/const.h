334cf2200a323ffd40e50f455aff28ff32b2a399
Santiago Salazar <salazars1@xavier.edu>
2019-10-17 17:43:28 -0400

    changed const

--- h/const.h
+++ h/const.h
@@ -114,8 +114,8 @@
 
 /* ON/OFF bit manipulation*/
 #define ALLOFF  0x00000000
-#define VMOFF   0xFDFFFFFF      /* Virtual Memory OFF*/
-#define VMON    0x02000000      /* Virtual Memory ON*/
+#define VMOFF   0xFEFFFFFF      /* Virtual Memory OFF*/
+#define VMON    0x01000000      /* Virtual Memory ON*/
 #define UMOFF   0xFFFFFFF7      /* User Mode OFF (Kernel Mode ON)*/
 #define UMON    0x00000008      /* User Mode ON (Kernel Mode OFF)*/
 #define IMOFF   0xFFFF00FF      /* Interrupt UNMasked */
