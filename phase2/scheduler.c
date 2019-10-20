7d2accd12d242e1177411cdc58f0df2f868a1dd2
stonen2 <stonen2@xavier.edu>
2019-10-19 21:14:34 -0400

    Here we go again

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -83,6 +83,7 @@ void scheduler()
             /* Processor is twiddling its thumbs (JOBS WAITING FOR IO BUT NONE IN THE PROCESSQUEUE) */            
           /*Tested*/
             setSTATUS(ALLOFF | IEON | IECON | IMON);
+            setTIMER(10000000000);
             WAIT();
         }
 
