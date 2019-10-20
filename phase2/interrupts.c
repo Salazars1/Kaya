1198f786abcc843862e52b7cdc90f4a49e9001e5
stonen2 <stonen2@xavier.edu>
2019-10-19 23:07:12 -0400

    Here we go again

--- phase2/interrupts.c
+++ phase2/interrupts.c
@@ -192,7 +192,7 @@ testingbaby(14);
     {
         /*Terminal*/
 
-        if ((testing->t_transm_status & 0xF) != READY)
+        if ((testing->t_transm_status & 0xFF) != READY)
         {
                 
                 /*Acknowledge*/
