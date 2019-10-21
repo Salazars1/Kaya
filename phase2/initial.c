0cbf4c0e5cee5ca274216b450ca042574fbe58ad
stonen2 <stonen2@xavier.edu>
2019-10-21 12:16:57 -0400

    plase

--- phase2/initial.c
+++ phase2/initial.c
@@ -28,6 +28,8 @@ int softBlockCount;
 pcb_t *currentProcess;
 pcb_t *readyQue;
 int semD[SEMNUM];
+char okbuf[2048];			/* sequence of progress messages */
+
 
 extern void test(); 
 extern void addkbuf(char * strp);
@@ -111,3 +113,68 @@ int main()
 
     return 0;
 }
+
+
+
+
+
+/* This function prints a string on specified terminal and returns TRUE if 
+ * print was successful, FALSE if not   */
+unsigned int termprint(char * str, unsigned int term) {
+	memaddr * statusp;
+	memaddr * commandp;
+	devreg stat;
+	devreg cmd;
+	unsigned int error = FALSE;
+	
+	if (term < DEVPERINT) {
+		/* terminal is correct */
+		/* compute device register field addresses */
+		statusp = (devreg *) (TERM0ADDR + (term * DEVREGSIZE) + (TRANSTATUS * DEVREGLEN));
+		commandp = (devreg *) (TERM0ADDR + (term * DEVREGSIZE) + (TRANCOMMAND * DEVREGLEN));
+		
+		/* test device status */
+		stat = termstat(statusp);
+		if (stat == READY || stat == TRANSMITTED) {
+			/* device is available */
+			
+			/* print cycle */
+			while (*str != EOS && !error) {
+				cmd = (*str << CHAROFFSET) | PRINTCHR;
+				*commandp = cmd;
+
+				/* busy waiting */
+				stat = termstat(statusp);
+				while (stat == BUSY)
+					 stat = termstat(statusp);
+				
+				/* end of wait */
+				if (stat != TRANSMITTED)
+					error = TRUE;
+				else
+					/* move to next char */
+					str++;
+			} 
+		}
+		else
+			/* device is not available */
+			error = TRUE;
+	}
+	else
+		/* wrong terminal device number */
+		error = TRUE;
+
+	return (!error);		
+}
+
+
+/* This function placess the specified character string in okbuf and
+*	causes the string to be written out to terminal0 */
+extren svoid addokbuf(char *strp) {
+	char *tstrp = strp;
+	while ((*mp++ = *strp++) != '\0')
+		;
+	mp--;
+	termprint(tstrp, 0);
+}
+
