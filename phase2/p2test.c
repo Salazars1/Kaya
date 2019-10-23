52f803090c701a3518c9b005ad67c45735ec2d7c
Santiago Salazar <salazars1@xavier.edu>
2019-10-23 09:09:53 -0400

    p3

--- phase2/p2test.c
+++ phase2/p2test.c
@@ -152,14 +152,14 @@ void test() {
 	p2state.s_pc = p2state.s_t9 = (memaddr)p2;		/* p2 starts executing function p2 */
 	p2state.s_status = p2state.s_status | IEPBITON | CAUSEINTMASK;
 		
-	print("BEFORE TERMINATE THREAD\n");
+
 	STST(&p3state);
 
 	p3state.s_sp = p2state.s_sp - QPAGE;
 	p3state.s_pc = p3state.s_t9 = (memaddr)p3;
 	p3state.s_status = p3state.s_status | IEPBITON | CAUSEINTMASK;
 	
-	print("BEFORE TERMINATE THREAD\n");
+	
 	STST(&p4state);
 
 	p4state.s_sp = p3state.s_sp - QPAGE;
@@ -325,7 +325,7 @@ void p2() {
 	p1p2synch = 1;				/* p1 will check this */
 
 	SYSCALL(VERHOGEN, (int)&endp2, 0, 0);				/* V(endp2)     */
-	
+	print("_________________________\n");
 	SYSCALL(TERMINATETHREAD, 0, 0, 0);			/* terminate p2 */
 
 	/* just did a SYS2, so should not get to this point */
