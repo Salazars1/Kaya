0a2d5aa00088931f918d3cd11fcc7f1c23b57170
stonen2 <stonen2@xavier.edu>
2019-10-23 19:08:59 -0400

    Fixing SYs Call 7

--- phase2/p2test.c
+++ phase2/p2test.c
@@ -230,7 +230,7 @@ void test() {
 	/* make sure we really blocked */
 	if (p1p2synch == 0)
 		print("error: p1/p2 synchronization bad\n");
-	
+	print("we are here");
 	SYSCALL(CREATETHREAD, (int)&p3state, 0, 0);				/* start p3     */
 		print("p3 is started\n");
 
