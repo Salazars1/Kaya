70002d350b19c91da6760c7f3a418df3eb1fcdd4
Santiago Salazar <salazars1@xavier.edu>
2019-10-22 23:41:06 -0400

    sys1 refactoring

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -178,7 +178,6 @@ HIDDEN void Syscall1(state_t *caller)
         /*We did not have any more processses able to be made so we send back a -1*/
         /*addokbuf("No More processes left load state\n");*/
         caller->s_v0 = -1;
-        LDST(caller);
     }
     else
     {
@@ -197,8 +196,8 @@ HIDDEN void Syscall1(state_t *caller)
         /*WE were able to allocate thus we put 0 in the v0 register*/
         caller->s_v0 = 0;
         /*addokbuf("Load state and we done\n");*/
-        LDST(caller);
     }
+    LDST(caller);
 }
 
 /*  This services causes the executing process to be anihilated along with all its children, grand
