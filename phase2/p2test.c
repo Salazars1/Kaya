bea0a2f086269d9e308b7791b226fe4c1baa9066
Santiago Salazar <salazars1@xavier.edu>
2019-10-23 20:38:08 -0400

    p2test

--- phase2/p2test.c
+++ phase2/p2test.c
@@ -339,7 +339,7 @@ void p3() {
 
 	time1 = 0;
 	time2 = 0;
-
+print("HI NICK2-----------------------------");
 	/* loop until we are delayed at least half of clock V interval */
 	while (time2-time1 < (CLOCKINTERVAL >> 1) )  {
 		STCK(time1);			/* time of day     */
