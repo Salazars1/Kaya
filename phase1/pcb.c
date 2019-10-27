5fa27e22e21361c122a91007a586fb7c03fa7716
stonen2 <stonen2@xavier.edu>
2019-10-26 20:48:01 -0400

    Push changes to out child We need to watch out for the only child in the list!

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -369,6 +369,17 @@ pcb_PTR outChild(pcb_PTR p){
         return p; 
     }
 
+
+/*Only CHild*/
+    if(p->p_prevSib == NULL && p->p_nextSib == NULL && p = p->p_prnt ->p_child){
+        p->p_prnt ->p_child = NULL; 
+        p-> p_prnt = NULL; 
+        return p; 
+
+
+    }
+
+
     return NULL; 
 
 }
