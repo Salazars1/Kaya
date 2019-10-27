ae1576d56cd5c94356689bb9fe03c3dee21dbb42
stonen2 <stonen2@xavier.edu>
2019-10-26 23:53:23 -0400

    Clean this bitch

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -157,7 +157,7 @@ void SYSCALLHandler()
     /*make a new process and allocate a process block to it */
     pcb_t *birthedProc = allocPcb();
     /*Clean the new process block just in case*/
-   /* birthedProc = clean(birthedProc);*/
+    birthedProc = clean(birthedProc);
     /*If the new process is null then We know there is no way to allocate a process*/
     if (birthedProc != NULL)
     { 
