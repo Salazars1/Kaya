fc06409168ad9266e39c3d2f96d75561c9243f90
stonen2 <stonen2@xavier.edu>
2019-12-13 14:22:57 -0500

    I am not good

--- phase3/vmIOsupport.c
+++ phase3/vmIOsupport.c
@@ -89,7 +89,7 @@ void pager()
     debugPager2(checkthisid);
   
 
-    if(checkthisid > 2 || checkthisid < 3){
+    if((checkthisid > 2) || (checkthisid < 3)){
 
         /*SEnd there asses to the shadow realm */
         SYSCALL(SYSCALL2, 0,0,0);
