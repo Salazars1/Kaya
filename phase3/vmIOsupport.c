32bbfbe4c9fa47304c4eb40a9f5fd2c01fa6e5b8
Santiago Salazar <salazars1@xavier.edu>
2019-12-13 15:18:28 -0500

    User mode fuck

--- phase3/vmIOsupport.c
+++ phase3/vmIOsupport.c
@@ -176,7 +176,7 @@ void pager()
 
     /*Release mutex and return control to process */        
     SYSCALL(SYSCALL3, (int)&swapSem, 0, 0);
-    setSTATUS(ALLOFF | IMON | IEON | TEON | VMOFF | UMOFF);
+    setSTATUS(ALLOFF | IMON | IEON | TEON | VMOFF);
     debugPager2(23);
     debugPager(123); 
     LDST(oldState);
