94293dd52651c69d756409f1c8a71fa1f9ca3ff6
stonen2 <stonen2@xavier.edu>
2019-12-12 20:41:28 -0500

    Fuck these pointers

--- phase3/initProc.c
+++ phase3/initProc.c
@@ -194,7 +194,7 @@ void uProcInit()
     state_t* newStateTLB;
     state_t* newStatePRG;
     state_t* newStateSYS;
-    state_t stateProc;
+    state_t * stateProc;
     /*Set the memory addresses of the TLB PROGRAM TRAP AND SYS HANDLER*/
     memaddr TLBTOP;
     memaddr PROGTOP;
