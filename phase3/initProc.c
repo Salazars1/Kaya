94a6fdf8d07e821cbc343baea77b42c7ef8217b2
stonen2 <stonen2@xavier.edu>
2019-12-13 14:56:36 -0500

    added a bunch of comments

--- phase3/initProc.c
+++ phase3/initProc.c
@@ -47,6 +47,7 @@ HIDDEN void uProcInit();
 void debug(int a){}
 
 /*Called in the Initial.c File from phase2 (Our Main)*/
+/*TODO:*/
 void test()
 {
     /*Looping variables*/
@@ -75,6 +76,7 @@ void test()
     */
    kuSeg3.header = (0x2A<<24)|KUSEGSIZE;
    for(i=0;i<KUSEGSIZE;i++){
+       /*Set the Page tables */
        KSegOS.pteTable[i].entryHI = ((0xC0000 + i)<< 12)|(ZERO <<6);
        KSegOS.pteTable[i].entryLO = ALLOFF | DIRTY | GLOBAL;
     }
@@ -109,14 +111,12 @@ void test()
     /*MasterSema4
         -init to 0
     */
-   /*This semaphore is a mutual exclusion semaphore and is thus set to 0 */
-    masterSem = 0;
+    masterSem = 0;      /*This semaphore is a mutual exclusion semaphore and is thus set to 0 */
 
    /*Process initialization loop (for (i=1; i<MAXUPROC+1;i++)){
        --initialize stuff
        --SYS 1
-    }   
-    */
+    }*/
     for(i =1; i< MAXUPROC+1;i++){
         /* i becomes the ASID (processID)*/
         uProcs[i-1].UProc_pte.header = (0x2A<<24)|KUSEGSIZE;
@@ -134,7 +134,6 @@ void test()
         }
 
         /*fix the last entry's entryHi = 0xBFFFF w/asid*/
-        /*Can set to 31 AFTER code works*/
         uProcs[i-1].UProc_pte.pteTable[KUSEGSIZE-1].entryHI = (0xBFFFF << 12) | (i << 6); 
 
         /*Set up the appropiate three entries in the global segment table
@@ -158,21 +157,18 @@ void test()
             -status: all interrupts enabled, local timer enabled, VM off, kernel mode on
         */
         procState.s_asid= (i<<6);              /*Set the asid*/
-        /*Math will have to change, Just 2 * 4096 No need for Max processes*/
         procState.s_sp = ALLOCATEHERE + ((i-1) * BASESTACKALLOC);            /*Take the address of the the base that we can allocate then allocate a unique address with 2 pages of memory */
         procState.s_pc = (memaddr) uProcInit;
         procState.s_t9 = (memaddr) uProcInit;
         procState.s_status = ALLOFF | IEON | IMON | TEBITON;
         uProcs[i-1].UProc_semAdd = 0; 
 
-
         /*Create Process*/
         SYSCALL(SYSCALL1, (int)&procState, 0, 0);
     }
 
     /*for (i=0; i<MAXUPROC; i++){
-        P(MasterSema4);
-    }
+        P(MasterSema4);}
     */
    for (i = 0; i < MAXUPROC; i++)
    {
@@ -185,17 +181,19 @@ void test()
 
 }
 
+/*TODO:*/
 void uProcInit()
 {
-
     /*Set a series of local variables*/
     unsigned int asid;
     int deviceNo;
+    
     /*Set states for the TLB PROGRAM TRAP AND SYS HANDLER*/
     state_t* newStateTLB;
     state_t* newStatePRG;
     state_t* newStateSYS;
     state_t  stateProc;
+    
     /*Set the memory addresses of the TLB PROGRAM TRAP AND SYS HANDLER*/
     memaddr TLBTOP;
     memaddr PROGTOP;
@@ -204,6 +202,15 @@ void uProcInit()
     /*Figure out who you are? ASID?*/
     asid = getENTRYHI();
     asid = (asid & 0x00000FC0) >> 6;
+     
+    /*Calculating program tops*/
+    PROGTOP = ALLOCATEHERE + ((asid-1) * BASESTACKALLOC);
+    SYSTOP = ALLOCATEHERE + ((asid-1) * BASESTACKALLOC);
+    TLBTOP = PROGTOP - PAGESIZE;
+
+    state_t * oldStateTLB; 
+    state_t* oldStatePRG; 
+    state_t * oldStateSYS; 
     
     /*Set up three new areas for Pass Up or Die
         -stack page: to be filled in later
@@ -212,16 +219,6 @@ void uProcInit()
         -status: all interrupts enabled, local timer enabled, VM ON, Kernel Mode ON
     */
 
-   /*Works for 1 process will need to change later on!*/
- 
-    PROGTOP = ALLOCATEHERE + ((asid-1) * BASESTACKALLOC);
-    SYSTOP = ALLOCATEHERE + ((asid-1) * BASESTACKALLOC);
-    /*Sneaky Sneaky Sneaky */
-    TLBTOP = PROGTOP - PAGESIZE;
-    state_t * oldStateTLB; 
-    state_t* oldStatePRG; 
-    state_t * oldStateSYS; 
-    
     /*Create a Pager Handler*/
     newStateTLB = &(uProcs[asid-1].UProc_NewTrap[TLBTRAP]);
     oldStateTLB = &(uProcs[asid-1].UProc_OldTrap[TLBTRAP]);
@@ -229,8 +226,7 @@ void uProcInit()
     newStateTLB->s_pc = (memaddr) pager;
     newStateTLB->s_t9 = (memaddr) pager;
     newStateTLB->s_asid = (asid << 6);
-    newStateTLB->s_status = ALLOFF | IMON | IEON | TEON | VMOFF | UMOFF;
-
+    newStateTLB->s_status = ALLOFF | IMON | IEON | TEON | VMOFF;
 
     /*Create a Program Trap handler state*/
     newStatePRG = &(uProcs[asid-1].UProc_NewTrap[PROGTRAP]);
@@ -240,7 +236,6 @@ void uProcInit()
     newStatePRG->s_t9 = (memaddr) uPgmTrpHandler;
     newStatePRG->s_asid = (asid << 6);
     newStatePRG->s_status = ALLOFF | IMON | IEON | TEON | VMON2 ;
-
 
     /*Create a Syshandler State*/
     newStateSYS = &(uProcs[asid-1].UProc_NewTrap[SYSTRAP]);
@@ -260,13 +255,16 @@ void uProcInit()
 
    /*Read the content of the tape devices(asid-1) on the the backing store device (disk0)
        keep reading until the tape block marker (data1) is no longer ENDOFBLOCK
-       read block from tape and then write it out to disk0
-   */
+       read block from tape and then write it out to disk0*/
+
+    /*Mutual exclusion on the device*/
     SYSCALL(SYSCALL4, (int) &mutexArr[deviceNo], 0, 0);
 
+    /*Device declaration*/
     device_t* tape;
     device_t* disk;
     devregarea_t * Activedev;
+
     int buffer;
     int pageNumber;
     unsigned int tapeStatus;
@@ -278,7 +276,6 @@ void uProcInit()
     disk = &(Activedev -> devreg[0]);           /*Backing Store is at Number 0 !*/
     tape = &(Activedev ->devreg[deviceNo]);     /*The tape is a dynamic number */
 
-
     /*Setting variables to be used in reading into tape and placing on backing store (Disk 0) */
     int bool = 0;
     tapeStatus= READY;
@@ -287,7 +284,7 @@ void uProcInit()
 
     /* loop until whole file has been read */
 	while((tapeStatus==READY) && !finished) {
-        /*Atomic operation*/
+        /*Atomic operation (READ FROM TAPE)*/
         InterruptsOnOff(FALSE);
 		    tape -> d_data0 = (ROMPAGESTART + (30 * PAGESIZE)) + ((asid - 1) * PAGESIZE);
 		    tape -> d_command = DISKREADBLK;
@@ -297,7 +294,7 @@ void uProcInit()
         /*MUTUAL EXCLUSION ON DISK*/
         SYSCALL(SYSCALL4, (int) &mutexArr[0], 0, 0);
 
-        /*Atomic operation*/
+        /*Atomic operation (IS THE DISK READY?)*/
         InterruptsOnOff(FALSE);
             disk ->d_command = (pageNumber << 8 | 2);
             diskStatus = SYSCALL(SYSCALL8, DISKINT, 0, 0);
@@ -305,14 +302,14 @@ void uProcInit()
         
         if(diskStatus == READY)
         {
-            /*Atomic operation*/
+            /*Atomic operation (WRITE IT ONTO THE DISK)*/
             InterruptsOnOff(FALSE);
                 disk->d_data0 = (ROMPAGESTART + (30 * PAGESIZE)) + ((asid - 1) * PAGESIZE);
                 disk->d_command = ((asid - 1) << 8) | 4;
                 diskStatus = SYSCALL(SYSCALL8,DISKINT,0,0);
             InterruptsOnOff(TRUE);
         }
-        /*MUTUAL EXCLUSION ON DISK*/
+        /*RELEASE MUTUAL EXCLUSION ON DISK*/
         SYSCALL(SYSCALL3, (int) &mutexArr[0], 0, 0);
 
         if(tape->d_data1 != 2){
@@ -320,31 +317,25 @@ void uProcInit()
         }
         pageNumber++;
 	}
-    
+
+    SYSCALL(SYSCALL3, (int) &mutexArr[deviceNo], 0, 0);
+
     /*Set up a new state for the user process
         -asid = your asid
         -stack page = last page of KUseg2(0C00.0000)
         -status: all interrupts enabled, local timer enabled, VM ON, User mode ON
         -PC = well known address from the start of KUseg2
     */
-   SYSCALL(SYSCALL3, (int) &mutexArr[deviceNo], 0, 0);
+
    /*Establish a new state that points the "Pager" Such that we can handle virtual memory*/
-    debug(asid);
     stateProc.s_asid = asid << 6;
-    debug(4);
     stateProc.s_sp = (memaddr) SEG3;
-    debug(5);
     stateProc.s_status = ALLOFF | IEON | IMON | TEBITON | UMOFF | VMON2;
-    debug(6);
     stateProc.s_pc = (memaddr) WELLKNOWNSTARTPROCESS; 
     stateProc.s_t9 = (memaddr) WELLKNOWNSTARTPROCESS;
-    debug(7);
-   debug(1);
     
    /*Loadstate to the new state that we have just created*/
    LDST(&stateProc);
-   
-
 }
 
 /*Function to toggle interrupts on and off*/
