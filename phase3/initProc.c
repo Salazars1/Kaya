c621fcc270e296687c804dd27e24993c7ce2403f
Santiago Salazar <salazars1@xavier.edu>
2019-12-14 02:32:52 -0500

    VMON

--- phase3/initProc.c
+++ phase3/initProc.c
@@ -341,7 +341,7 @@ void uProcInit()
     stateProc.s_asid = asid << 6;
     stateProc.s_sp = (memaddr) SEG3;
     stateProc.s_status = ALLOFF | IEON | IMON | TEBITON | UMOFF | VMON2;
-    stateProc.s_pc = (memaddr) WELLKNOWNSTARTPROCESS;
+    stateProc.s_pc = (memaddr) WELLKNOWNSTARTPROCESS; 
     stateProc.s_t9 = (memaddr) WELLKNOWNSTARTPROCESS;
     
    /*Loadstate to the new state that we have just created*/
